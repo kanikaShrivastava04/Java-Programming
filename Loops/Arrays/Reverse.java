f1747a396f24f9514c527d3d2618ca10e5670fc8
kanika <shrivastavakanika104@gmail.com>
2026-01-10 12:32:54 +0530

    Reverse

---
+++ Loops/Arrays/Reverse.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class Reverse {
+    public static void main(String[] args) {
+        int arr[]={1,3,2,4,5};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
